5ea32848b047caabab608e737f799ebfcf2e7b31
Sam Venter <sventer444@gmail.com>
2022-08-30 13:59:58 -0400

    overlayed detected lines

--- src/lanes.py
+++ src/lanes.py
@@ -8,19 +8,35 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1,y1), (x2, y2), (255, 0 , 0), 10)
+    return line_image
+
 def region_of_interest(image):
+    # Line Equation row = xcosTheta + ycosTheta
     height = image.shape[0]
     polygons = np.array([
         [(200, height), (1100, height), (550, 250)]
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons, 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 image = cv2.imread('img/test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-cv2.imshow('result', region_of_interest(canny))
+cropped_image = region_of_interest(canny)
+# When selecting scope for Hough bins, too big is too general and too small wont be accurate
+# Maybe try small and have a recheck with higher number? Or start medium and try scoping in if possible
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
 
 
